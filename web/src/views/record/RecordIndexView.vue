f29bdbddde3ee8907a11008d4b3a5d889f453860
thbo chou <3186995001@qq.com>
2024-07-19 00:18:08 +0800

    导航栏实现

---
+++ web/src/views/record/RecordIndexView.vue
@@ -0,0 +1,16 @@
+<template>
+    <ContentField>对战列表</ContentField>
+</template>
+
+<script>
+import ContentField from '@/components/ContentField.vue'
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
